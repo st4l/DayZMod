f642da9da04ace3754f732cd49acb58b32123172
UnclearWall <unclearwall@gmail.com>
2012-11-05 10:42:16 -0600

    Updated Vehicle Class

--- @dayz/dayz_code/cfgVehicles.hpp
+++ @dayz/dayz_code/cfgVehicles.hpp
@@ -194,20 +194,14 @@ class CfgVehicles {
 		gunnerCanSee = 2+16+32;
 		driverCanSee = 2+16+32;
 	};
-	//An2_1_TK_CIV_EP1
-	class An2_1_TK_CIV_EP1 {
+	//Mi17_Civilian
+	class Mi17_Civilian {
 		radartype=0;
+		weapons[] = {};
+		magazines[] = {};
 	};
-	//An2_2_TK_CIV_EP1
-	class An2_2_TK_CIV_EP1 {
-		radartype=0;
-	};
-	//An2_1_TK_CIV_EP1
+	//An2_TK_EP1
 	class An2_TK_EP1 {
-		radartype=0;
-	};
-	//MV22
-	class MV22 {
 		radartype=0;
 		weapons[] = {};
 		magazines[] = {};
